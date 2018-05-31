76cdd475f50d6c90acea14ed2c7527f96fa2a506
Wojciech Urbański <wojciech.urbanski@sentione.com>
2018-06-01 00:24:05 +0200

    14. Using slots to extract the logic of showing a loading indicator.

---
+++ src/components/LoadingStatus.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div v-if="!!isLoading" class="spinner"></div>
+    <slot v-else />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      isLoading: {
+        default: false
+      }
+    }
+  }
+</script>
