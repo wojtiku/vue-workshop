3a580ce1202f1731c7e0bd69afb85bd7db98c424
Wojciech Urbański <wojciech.urbanski@sentione.com>
2018-06-01 09:21:50 +0200

    22. Products list is now 100% driven by the store - it also handles data fetching via actions

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import {getAllProducts} from '/src/productService';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     products: [],
+    productsStatus: {},
   },
   getters: {
     currentPageNumber: (state, getters, rootState) => {
@@ -15,15 +17,29 @@ export default new Vuex.Store({
       return 1
     },
     products: (state) => state.products,
+    productsStatus: (s) => s.productsStatus
   },
   mutations: {
     updateProducts(state, newProducts) {
       state.products = newProducts;
+    },
+    changeProductsStatus(state, newProductsStatus) {
+      state.productsStatus = newProductsStatus;
     }
   },
   actions: {
     updateProducts({ commit }, newProducts) {
       commit("updateProducts", newProducts);
+      commit("changeProductsStatus", { loading: false });
     },
+    fetchProducts({ commit, dispatch, getters }) {
+      commit("changeProductsStatus", { loading: true });
+      return getAllProducts(getters.currentPageNumber)
+        .then((products => dispatch("updateProducts", products)))
+        .catch((e) => {
+          dispatch("updateProducts", []);
+          commit("changeProductsStatus", { error: e });
+        });
+    }
   }
 });
