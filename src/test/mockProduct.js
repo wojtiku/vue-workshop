1f87d6cc57de427ea2403b7a0c836afe91146974
Wojciech Urbański <wojciech.urbanski@sentione.com>
2018-06-02 18:20:21 +0200

    31. Testing components using vuex having subcomponents.

--- src/test/mockProduct.js
+++ src/test/mockProduct.js
@@ -12,3 +12,64 @@ export default {
     "Plastic"
   ]
 };
+
+
+export const mockProductList = [
+  {
+    "id": 1,
+    "name": "Intelligent Soft Computer",
+    "price": "280.00",
+    "department": "Health",
+    "color": "#126123",
+    "description": "Est occaecati porro. Qui voluptatum nihil et voluptatem ea. Ut repellendus dicta velit quisquam totam sequi dolorum. Eius voluptatem asperiores consequatur non iusto atque et mollitia. Omnis sequi repudiandae.",
+    "photo": "/product-photos/2.jpeg",
+    "inStock": 7,
+    "materials": [
+      "Fresh"
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Awesome Granite Salad",
+    "price": "809.00",
+    "department": "Movies",
+    "color": "#402a23",
+    "description": "Ut ut dolores ut aut officia qui nemo eum voluptatem. Voluptas dolores qui voluptatem consequatur quam itaque. Ullam voluptatem in dolorem repellat non aut vero ea.",
+    "photo": "/product-photos/3.jpeg",
+    "inStock": 10,
+    "materials": [
+      "Rubber"
+    ]
+  },
+  {
+    "id": 4,
+    "name": "Rustic Cotton Cheese",
+    "price": "601.00",
+    "department": "Health",
+    "color": "#18047b",
+    "description": "Et inventore qui doloremque vel. Ut nulla quae nobis aut. Ipsam ut impedit sed consequatur illo neque alias ipsum.",
+    "photo": "/product-photos/4.jpeg",
+    "inStock": 6,
+    "materials": [
+      "Wooden",
+      "Metal",
+      "Frozen",
+      "Soft",
+      "Rubber"
+    ]
+  },
+  {
+    "id": 5,
+    "name": "My updated product",
+    "price": "219.00",
+    "department": "Home",
+    "color": "#51452b",
+    "description": "Consequatur nam distinctio modi et omnis odio optio. Ut ipsum voluptatem. Voluptatem est voluptatem a. Sed et et sunt aut. Error hic dolor quas velit aut omnis repudiandae.",
+    "photo": "/product-photos/5.jpeg",
+    "inStock": 3,
+    "materials": [
+      "Granite",
+      "Plastic"
+    ]
+  }
+];
