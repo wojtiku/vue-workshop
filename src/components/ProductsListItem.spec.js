3770b94fcff66c4ddb3b7289e90b51c9998bd093
Wojciech Urbański <wojciech.urbanski@sentione.com>
2018-06-02 11:25:29 +0200

    29. Testing functional components, stubbing components.

---
+++ src/components/ProductsListItem.spec.js
@@ -0,0 +1,57 @@
+import ProductsListItem from '/src/components/ProductsListItem.vue';
+import mockProduct from '/src/test/mockProduct';
+import {mount, RouterLinkStub} from '@vue/test-utils';
+
+function instantiateWithProduct(partialProduct) {
+  return mount(ProductsListItem, {
+    context: {
+      props: {
+        product: { ...mockProduct, ...partialProduct }
+      }
+    },
+    stubs: {
+      RouterLink: RouterLinkStub
+    },
+    directives: {
+      "style-when-broken": () => {}
+    }
+  });
+}
+
+describe('ProductsListItem.vue', () => {
+
+  it('renders props.product.name when passed', () => {
+    const name = "awesome product name";
+    const vm = instantiateWithProduct({ name });
+    expect(vm.html()).toContain(name);
+  });
+
+  describe('Highlights product', () => {
+    it('should be highlighted if price is below 300', () => {
+      const vm = instantiateWithProduct({ price: 299 });
+      expect(vm.classes()).toContain("highlight");
+    });
+
+    it('should not be highlighted if price is 300 or more', () => {
+      const vm = instantiateWithProduct({ price: 300 });
+      expect(vm.classes()).not.toContain("highlight");
+    });
+  });
+
+  describe("Hides footer", () => {
+    const footerPriceSelector = '.product-list--product--footer .product-list--product--price';
+    const footerLozengeSelector = '.product-list--product--footer > .lozenge';
+
+    it('should show the footer when product is in stock', () => {
+      const vm = instantiateWithProduct({ inStock: 1 });
+      expect(vm.find(footerPriceSelector).exists()).toBe(true);
+      expect(vm.find(footerLozengeSelector).exists()).toBe(false);
+    });
+
+    it('should hide the footer when product is NOT in stock', () => {
+      const vm = instantiateWithProduct({ inStock: 0 });
+      expect(vm.find(footerPriceSelector).exists()).toBe(false);
+      expect(vm.find(footerLozengeSelector).exists()).toBe(true);
+    });
+  });
+});
---
+++ src/test/mockProduct.js
@@ -0,0 +1,14 @@
+export default {
+  "id": 0,
+  "name": "My updated product",
+  "price": "219.00",
+  "department": "Home",
+  "color": "#51452b",
+  "description": "Consequatur nam distinctio modi et omnis odio optio. Ut ipsum voluptatem. Voluptatem est voluptatem a. Sed et et sunt aut. Error hic dolor quas velit aut omnis repudiandae.",
+  "photo": "/product-photos/1.jpeg",
+  "inStock": 3,
+  "materials": [
+    "Granite",
+    "Plastic"
+  ]
+};
